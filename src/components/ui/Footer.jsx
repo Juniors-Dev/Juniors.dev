82d9d8fc08c2284cac7d6828a4b6b8579069f91c
HelChris <111705707+HelChris@users.noreply.github.com>
2025-10-13 13:41:42 +0200

    fix: trying to add correct css for lightmode/darkmode. Icons looking correct in default lightmode

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,32 +1,41 @@
 function Footer() {
   return (
-    <footer class="text-center flex-row gap-6">
-      <h5 class="text-[var(--h5-size)] bold">
-        Juniors<img src="src/assets/ducky1.svg" class="w-0.5 inline align-baseline"></img>dev
+    <footer className="text-center flex-row gap-6">
+      <h5 className="text-[var(--h5-size)] bold">
+        Juniors<img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline"></img>dev
       </h5>
-      <p class="text-[var(--text-caption-size)] bold">We Build, Work and Win Together</p>
-      <p class="text-[var(--text-body-size)]">Follow Us</p>
-      <div class="flex gap-6 py-4 justify-center">
+      <p className="text-[var(--text-caption-size)] bold">We Build, Work and Win Together</p>
+      <p className="text-[var(--text-body-size)]">Follow Us</p>
+      <div className="flex gap-6 py-4 justify-center">
         {/* <img
           src="src/assets/discord-simple.svg"
           alt="discord link"
-          class="invert-100 w-6 h-6"
+          class=" w-6 h-6"
         ></img> */}
         <img
           src="src/assets/facebook-simple.svg"
           alt="facebook link"
-          class="invert-100 w-6 h-6"
+          className="w-6 h-6 icon"
         ></img>
         <img
           src="src/assets/instagram-simple.svg"
           alt="instagram link"
-          class="invert-100 w-6 h-6"
+          className="w-6 h-6 icon"
         ></img>
-        <img src="src/assets/linkedin-darkmode.png" alt="linkedin link" class="w-6 h-6"></img>
-        <img src="src/assets/github-simple.svg" alt="github link" class="invert-100 w-6 h-6"></img>
+        <img
+          src="src/assets/linkedin-lightmode.png"
+          alt="linkedin link"
+          className="w-6 h-6 dark:hidden"
+        ></img>
+        <img
+          src="src/assets/linkedin-darkmode.png"
+          alt="linkedin link"
+          className="w-6 h-6 hidden dark:block"
+        ></img>
+        <img src="src/assets/github-simple.svg" alt="github link" className="w-6 h-6 icon"></img>
       </div>
-      <div class="pb-4">Language toggle</div>
-      <p class="text-[var(--text-caption-size)]">© 2025 Juniors.dev | All rights reserved</p>
+      <div className="pb-4">Language toggle</div>
+      <p className="text-[var(--text-caption-size)]">© 2025 Juniors.dev | All rights reserved</p>
     </footer>
   );
 }
