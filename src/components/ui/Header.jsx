5e58a978de87829ad01d377dc3d6f4e46135b23f
HelChris <111705707+HelChris@users.noreply.github.com>
2025-10-27 20:42:04 +0100

    style: add justify-center

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <header>
-      <div className="">
-        <h5 className="text-[var(--h5-size)] bold">
+      <div className="flex justify-center">
+        <h5 className="text-[var(--h5-size)]">
           Juniors<img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline"></img>dev
         </h5>
       </div>
