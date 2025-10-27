5579e28d6d76a1b21a55caf61f24927323a36aeb
HelChris <111705707+HelChris@users.noreply.github.com>
2025-10-27 20:46:37 +0100

    style: return header before themeToggle

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ function App() {
   }, []);
   return (
     <>
+      <Header />
       <div className="fixed top-4 right-4 z-50">
         <ThemeToggle />
       </div>
-      <Header />
       <main>
         <Hero />
         <Mission />
