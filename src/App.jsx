098c7f87338adb80a555e2e3e63ebf1c031388a8
HelChris <111705707+HelChris@users.noreply.github.com>
2025-10-27 20:42:59 +0100

    style: add to right side

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   }, []);
   return (
     <>
-      <div className="fixed top-4 left-4 z-50">
+      <div className="fixed top-4 right-4 z-50">
         <ThemeToggle />
       </div>
       <Header />
