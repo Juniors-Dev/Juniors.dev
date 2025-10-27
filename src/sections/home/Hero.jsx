deee71464349d5bdb375896e25801f63b660b25c
HelChris <111705707+HelChris@users.noreply.github.com>
2025-10-27 21:13:44 +0100

    feat: add Hero

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,7 +1,16 @@
 function Hero() {
   return (
     <section>
-      <h2>Hero</h2>
+      <div className="flex flex-col items-center">
+        <img src="/src/assets/placeholder-img.png" alt="placeholder image" />
+        <h1 className="font-[var(--h1-size)] text-[var(--color-primary-nightwing-hex)] text-center text-pretty">
+          We build, <br />
+          Work and Win Together
+        </h1>
+        <p className="font-[var(--h2-size)] text-[var(--color-primary-nightwing-hex)] text-center text-pretty">
+          Empowering developers through community, collaboration and career opportunities.
+        </p>
+      </div>
     </section>
   );
 }
