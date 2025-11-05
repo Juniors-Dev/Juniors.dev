9ff2dac22c3cc022f8cd03bf9ffc9d100c42ebb4
HelChris <111705707+HelChris@users.noreply.github.com>
2025-11-05 15:38:53 +0100

    feat: add Hero

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -1,20 +1,31 @@
-//import button when the PR is approved
+import Button from "/src/components/button/Button";
 
-//to do:
-// add spacing, button which scrolls down to the contact form on the bottom
-// button = primary, text = contact us
 function Hero() {
+  const scrollToContact = () => {
+    document.getElementById("contact-form")?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
     <section>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col baseline-start gap-4">
         <img src="/src/assets/placeholder-img.png" alt="placeholder image" />
-        <h1 className="font-[var(--h1-size)] text-[var(--color-primary-nightwing-hex)] text-center text-pretty">
-          We build, <br />
-          Work and Win Together
+        <h1 className="font-[var(--h1-size)] text-[var(--color-primary-nightwing-hex)] text-pretty text-left xs:text-center sm:text-center">
+          We build, <br className="sm:hidden" />
+          Work and Win <br className="sm:hidden" />
+          Together
         </h1>
-        <p className="font-[var(--h2-size)] text-[var(--color-primary-nightwing-hex)] text-center text-pretty">
-          Empowering developers through community, collaboration and career opportunities.
+        <p className="font-[var(--h2-size)] text-[var(--color-primary-nightwing-hex)] text-left text-pretty xs:text-center sm:text-center">
+          Empowering developers through community, <br className="hidden xs:block" />
+          collaboration and career opportunities.
         </p>
+        <div className="flex justify-center">
+          <Button variant="primary" size="md" onClick={scrollToContact}>
+            Contact Us
+          </Button>
+        </div>
       </div>
     </section>
   );
