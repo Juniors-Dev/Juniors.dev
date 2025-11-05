2f65a2c0cf84117a85f6c2005ee1a8f2238f13fa
Helene <111705707+HelChris@users.noreply.github.com>
2025-11-05 13:46:48 +0100

    style: align to the left

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <div className="flex justify-center">
+      <div className="flex items-start">
         <h5 className="text-[var(--h5-size)]">
           Juniors<img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline"></img>dev
         </h5>
