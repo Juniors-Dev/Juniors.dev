6ac800304a7c2c6526bb35526682ce033c0fb57f
HelChris <111705707+HelChris@users.noreply.github.com>
2025-11-24 23:07:44 +0100

    refactor: responsive w/xs breakpoint

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -3,16 +3,18 @@ import LanguageToggle from "./LanguageToggle";
 
 function Header() {
   return (
-    <header className="grid grid-cols-5 gap-1 xs:grid-cols-10">
+    <header className="grid grid-cols-5 xs:grid-cols-8 sm:grid-cols-10">
       <div className="col-span-3 self-center xs:col-span-4">
         <h5 className="text-h5">
-          Juniors<img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline"></img>dev
+          Juniors
+          <img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline" />
+          dev
         </h5>
       </div>
-      <div className="pr-6 col-start-4 xs:col-start-9 justify-self-end sm:p-0">
+      <div className="pr-8 col-start-4 xs:col-start-7 sm:col-start-9 sm:justify-self-end sm:p-0">
         <ThemeToggle />
       </div>
-      <div className="col-start-5 pl-6 sm:pl-0 xs:col-start-10 place-self-center justify-self-end ">
+      <div className="col-start-5 pl-8 sm:pl-0 xs:col-start-8 sm:col-start-10 place-self-center justify-self-end">
         <LanguageToggle />
       </div>
     </header>
