5cb921f99e5a1ff37b001debcfa2349b7d50a005
HelChris <111705707+HelChris@users.noreply.github.com>
2025-11-24 17:24:18 +0100

    refactor: include themetoggle and languagetoggle in header. Add grid to header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,10 +1,19 @@
+import ThemeToggle from "./ThemeToggle";
+import LanguageToggle from "./LanguageToggle";
+
 function Header() {
   return (
-    <header>
-      <div className="flex items-start">
-        <h5 className="text-[var(--h5-size)]">
+    <header className="grid grid-cols-5 gap-1 xs:grid-cols-10">
+      <div className="col-span-3 self-center xs:col-span-4">
+        <h5 className="text-h5">
           Juniors<img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline"></img>dev
         </h5>
+      </div>
+      <div className="pr-6 col-start-4 xs:col-start-9 justify-self-end sm:p-0">
+        <ThemeToggle />
+      </div>
+      <div className="col-start-5 pl-6 sm:pl-0 xs:col-start-10 place-self-center justify-self-end ">
+        <LanguageToggle />
       </div>
     </header>
   );
