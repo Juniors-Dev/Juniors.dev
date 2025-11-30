4541920029558396e3dd3763c624e24c58cc3347
HelChris <111705707+HelChris@users.noreply.github.com>
2025-11-30 16:11:09 +0100

    style: properly handle logo light/dark SVG

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -5,11 +5,16 @@ function Header() {
   return (
     <header className="grid grid-cols-5 xs:grid-cols-8 sm:grid-cols-10">
       <div className="col-span-3 self-center xs:col-span-4">
-        <h5 className="text-h5">
-          Juniors
-          <img src="src/assets/ducky1.svg" className="w-0.5 inline align-baseline" />
-          dev
-        </h5>
+        <img
+          src="/src/assets/SVG-logo-lightmode.svg"
+          className="block dark:hidden max-w-36"
+          alt="Juniors.dev logo. The dot in the logo is an image of a tiny yellow rubber duck."
+        />
+        <img
+          src="/src/assets/SVG-logo-darkmode.svg"
+          className="hidden dark:block max-w-36"
+          alt="Juniors.dev logo. The dot in the logo is an image of a tiny yellow rubber duck."
+        />
       </div>
       <div className="pr-8 col-start-4 xs:col-start-7 sm:col-start-9 sm:justify-self-end sm:p-0">
         <ThemeToggle />
