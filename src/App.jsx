302e095e0851d71f8c1077504b674e36aad7c5c7
Anclagen <91462616+Anclagen@users.noreply.github.com>
2026-04-01 00:34:15 +0200

    feat: transferred design variables for tailwind, cleared out most of the older code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useEffect } from "react";
 import Header from "./components/ui/Header";
 import Hero from "./sections/home/Hero";
-import Mission from "./sections/home/Mission";
-import Expect from "./sections/home/Expect";
-import Roadmap from "./sections/home/Roadmap";
-import Contact from "./sections/home/Contact";
 import Footer from "./components/ui/Footer";
 
 function App() {
-  useEffect(() => {
-    const metaDescription = document.querySelector('meta[name="description"]');
-    if (metaDescription) {
-      metaDescription.setAttribute(
-        "content",
-        "Welcome to Juniors.Dev, a creative community and collaboration hub built to give junior developers real-world experience through teamwork, hands-on learning, and ship-worthy projects."
-      );
-    } else {
-      const meta = document.createElement("meta");
-      meta.name = "description";
-      meta.content =
-        "Welcome to Juniors.Dev, a creative community and collaboration hub built to give junior developers real-world experience through teamwork, hands-on learning, and ship-worthy projects.";
-      document.head.appendChild(meta);
-    }
-  }, []);
   return (
     <>
+      <Header />
       <main>
-        <Header />
-        <Hero />
-        <Mission />
-        <Expect />
-        <Roadmap />
-        <Contact />
+        <Hero></Hero>
       </main>
       <Footer />
     </>
--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -11,16 +11,38 @@ function Hero() {
   return (
     <section>
       <div className="flex flex-col baseline-start gap-4">
-        <img src="/src/assets/placeholder-img.png" alt="placeholder image" />
-        <h1 className="text-h2 text-fg text-pretty text-left xs:text-center sm:text-center">
-          We build, <br className="sm:hidden" />
-          Work and Win <br className="sm:hidden" />
-          Together
+        {/* <img src="/src/assets/placeholder-img.png" alt="placeholder image" /> */}
+        <h1 className="text-h1 text-fg text-pretty text-left xs:text-center sm:text-center">
+          Heading 1
         </h1>
-        <p className="text-h4 text-fg text-left text-pretty xs:text-center sm:text-center">
+        <h2 className="text-h2 text-fg text-pretty text-left xs:text-center sm:text-center">
+          Heading 2
+        </h2>
+        <h3 className="text-h3 text-fg text-pretty text-left xs:text-center sm:text-center">
+          Heading 3
+        </h3>
+        <h3 className="text-subheading-1 text-fg text-pretty text-left xs:text-center sm:text-center">
+          Sub-Heading 1
+        </h3>
+        <h4 className="text-subheading-2 text-fg text-pretty text-left xs:text-center sm:text-center">
+          Sub-Heading 2
+        </h4>
+
+        <p className="text-body text-fg text-left text-pretty xs:text-center sm:text-center">
           Empowering developers through community, <br className="hidden xs:block" />
           collaboration and career opportunities.
         </p>
+
+        <p className="text-label text-fg text-left text-pretty xs:text-center sm:text-center">
+          Empowering developers through community, <br className="hidden xs:block" />
+          collaboration and career opportunities.
+        </p>
+        <div className="text-center">
+          <button className="btn">Base</button>
+          <button className="btn btn-primary">Primary</button>
+          <button className="btn btn-secondary">Secondary</button>
+          <button className="btn btn-nav">Nav</button>
+        </div>
         <div className="flex justify-center">
           <Button variant="primary" size="md" onClick={scrollToContact}>
             Contact Us
