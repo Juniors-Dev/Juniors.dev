6b88c00d5935ae3e455450e6afa6a2f96aac5106
MiaTexnes <miatex50428@stud.noroff.no>
2026-04-23 10:38:04 +0200

    refactor: simplify Projects page by integrating OurProjects component

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,23 +1,7 @@
-import { Section } from "../../features/UI";
-import ProjectCard from "../../features/ProjectCard/ProjectCard";
-import { featuredProjects } from "../Home/sections/OurProjects";
-import { projects } from "../Home/translations/projects";
-import { useT } from "../../stores/languageStore";
+import OurProjects from "../Home/sections/OurProjects";
 
 function Projects() {
-  const t = useT(projects);
-
-  return (
-    <Section>
-      <h1 className="text-h1 text-primary-800">{t.heading}</h1>
-
-      <div className="projects-grid mt-8 grid gap-6">
-        {featuredProjects.map((project) => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
-      </div>
-    </Section>
-  );
+  return <OurProjects />;
 }
 
 export default Projects;
