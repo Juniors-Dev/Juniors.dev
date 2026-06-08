3f81a12f0156245d76453c5790089e964a791d82
Frida Sand <163577510+fmSand@users.noreply.github.com>
2026-06-08 16:27:02 +0200

    fix: remove unused tailwind classes

--- src/pages/Home/sections/Services.jsx
+++ src/pages/Home/sections/Services.jsx
@@ -10,8 +10,8 @@ function OurServices() {
     <Section className="bg-primary-100 relative">
       <h2 className="text-primary-800">{t.heading}</h2>
 
-      <div className="relative services-section__container">
-        <div className="services-rail relative">
+      <div className="services-section__container">
+        <div className="services-rail">
           {serviceCards.map((card) => {
             const content = card[language];
 
